a89412e40967a80fa5184c01159af94d11c4fb68
Max Lacroix <m.lacroix@instadeep.com>
2025-08-21 15:01:07 +0200

    exp: jobset sucess fix

--- smoke-test/src/operators/jobset.py
+++ smoke-test/src/operators/jobset.py
@@ -5,6 +5,7 @@ def jobsetop(tb_write):
     print_jobset_env(tb_write)
 
 def print_jobset_env(tb_write):
-    print("job completion index: ", os.environ.get("JOB_COMPLETION_INDEX"))
+    jci = os.environ.get("JOB_COMPLETION_INDEX")
+    print(f"job completion index: {jci}", )
     if tb_write:
-        dummy_tb_write(f"From job_index {os.environ.get("JOB_COMPLETION_INDEX")}")
+        dummy_tb_write(f"From job_index {jci}")
